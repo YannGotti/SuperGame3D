667de9bceac5b0e131e47fce18f283f21856cde2
mironoouv <1111111111aleksey46@gmail.com>
2026-03-25 11:39:05 +0300

    update project

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -2,13 +2,25 @@ using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 3f;
+
+    public Vector2 CurrentInput { get; private set; }
+    public float NormalizedSpeed { get; private set; }
 
     public void Move(Vector2 deltaVector)
     {
-        if (deltaVector == Vector2.zero)
+        CurrentInput = deltaVector;
+
+        Vector3 move = new Vector3(deltaVector.x, 0f, deltaVector.y);
+        float inputMagnitude = Mathf.Clamp01(deltaVector.magnitude);
+        NormalizedSpeed = inputMagnitude;
+
+        if (inputMagnitude <= 0.01f)
             return;
 
-        transform.position += speed * Time.deltaTime * new Vector3(deltaVector.x, 0, deltaVector.y);
+        transform.position += move.normalized * speed * inputMagnitude * Time.deltaTime;
+
+        // Поворот в сторону движения
+        transform.forward = move.normalized;
     }
 }
---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(MoveController))]
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private MoveController moveController;
+
+    private static readonly int SpeedHash = Animator.StringToHash("Speed");
+    private static readonly int MoveXHash = Animator.StringToHash("MoveX");
+    private static readonly int MoveYHash = Animator.StringToHash("MoveY");
+    private static readonly int IsMovingHash = Animator.StringToHash("IsMoving");
+
+    private void Awake()
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        if (moveController == null)
+            moveController = GetComponent<MoveController>();
+    }
+
+    private void Reset()
+    {
+        animator = GetComponent<Animator>();
+        moveController = GetComponent<MoveController>();
+    }
+
+    private void Update()
+    {
+        Vector2 input = moveController.CurrentInput;
+        float speed = moveController.NormalizedSpeed;
+
+        animator.SetFloat(SpeedHash, speed);
+        animator.SetFloat(MoveXHash, input.x);
+        animator.SetFloat(MoveYHash, input.y);
+        animator.SetBool(IsMovingHash, speed > 0.01f);
+    }
+}
